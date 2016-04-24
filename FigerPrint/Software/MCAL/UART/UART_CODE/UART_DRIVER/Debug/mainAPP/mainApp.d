1a1294cf3226d800a66bb0bb6c1fd70601549c2f
mahmoud <engactivemedo@gmail.com>
2016-04-24 10:23:07 +0200

    TX &RX iNTERUPTS are tested sho3.l fnade2 keda todo : 1- test the system on h/w

--- FigerPrint/Software/MCAL/UART/UART_CODE/UART_DRIVER/Debug/mainAPP/mainApp.d
+++ FigerPrint/Software/MCAL/UART/UART_CODE/UART_DRIVER/Debug/mainAPP/mainApp.d
@@ -2,7 +2,8 @@ mainAPP/mainApp.d mainAPP/mainApp.o: ../mainAPP/mainApp.c \
   ../mainAPP/../headerfiles/types.h \
   ../mainAPP/../headerfiles/UART/UART_Interface.h \
   ../mainAPP/../headerfiles/DelayFiles/Delay_prog.c \
-  ../mainAPP/../headerfiles/DelayFiles/Delay.h
+  ../mainAPP/../headerfiles/DelayFiles/Delay.h \
+  ../mainAPP/../headerfiles/InteruptVector.h
 
 ../mainAPP/../headerfiles/types.h:
 
@@ -11,3 +12,5 @@ mainAPP/mainApp.d mainAPP/mainApp.o: ../mainAPP/mainApp.c \
 ../mainAPP/../headerfiles/DelayFiles/Delay_prog.c:
 
 ../mainAPP/../headerfiles/DelayFiles/Delay.h:
+
+../mainAPP/../headerfiles/InteruptVector.h:
--- FigerPrint/Software/MCAL/UART/UART_CODE/UART_DRIVER/mainAPP/mainApp.c
+++ FigerPrint/Software/MCAL/UART/UART_CODE/UART_DRIVER/mainAPP/mainApp.c
@@ -7,13 +7,30 @@
 #include"../headerfiles/types.h"
 #include"../headerfiles/UART/UART_Interface.h"
 #include"../headerfiles/DelayFiles/Delay_prog.c"
+#include "../headerfiles/InteruptVector.h"
+//for tx Interupt testing only
+#define PORTA                     *((volatile u8*)(0x3B))
+#define DDRA                      *((volatile u8*)(0x3A))
+void txINT(void){
+	static u8 local_u8oldVal=0xff;
+	local_u8oldVal^=0xff;
+	PORTA=local_u8oldVal;
+}
+void RxINT(void){
+	 u8 local_u8sendAndReciving;
+	UART_voidReadFromUDR(&local_u8sendAndReciving);
+	UART_voidwriteToUDR(local_u8sendAndReciving);
+}
 int main (void)
 {
-	u8 local_u8sendAndReciving=0x55;
+	DDRA=0xff;
+	UART_voidSetRX_ISR(RxINT);
+	UART_voidSetTX_ISR( txINT);
 	UART_VoidInit();
+	INTERUPTVECTOR_SETGLOPALINTERUPT;
 while(1){
-	UART_VoidSending(local_u8sendAndReciving);
-	UART_VoidResiving(&local_u8sendAndReciving);
+    	//UART_VoidSending(local_u8sendAndReciving);
+	//UART_VoidResiving(&local_u8sendAndReciving);
 	Delay(200);
 
 }//end of while loop
